d23784fec272c7d02e14238421103f8fdda65ea8
GalsanSanzhiev <galsans054@yahoo.com>
2025-04-01 15:15:22 +0700

    Add files via upload

--- Макет №3/js/main.js
+++ Макет №3/js/main.js
@@ -3,16 +3,44 @@
 
     const burger = document.querySelector(".burger-icon");
     const nav = document.querySelector(".nav");
+    const body = document.querySelector(".body");
 
     burger.addEventListener('click', function () {
         this.classList.toggle("active");
         nav.classList.toggle("open");
+
+
+        if (nav.classList.contains("open")) {
+            body.classList.add("body--opened-menu");
+        } else {
+            body.classList.remove("body--opened-menu");
+        }
     });
 
     document.querySelectorAll(".nav__link").forEach(link => {
         link.addEventListener('click', () => {
             burger.classList.remove("active");
             nav.classList.remove("open");
+            body.classList.remove("body--opened-menu");
         });
     });
 })();
+
+const open = document.querySelector(".about__img-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".modal__cancel");
+
+open.addEventListener('click', function () {
+    modal.classList.add("active");
+    document.body.classList.add("body--opened-modal");
+})
+close.addEventListener('click', function () {
+    modal.classList.remove("active");
+    document.body.classList.remove("body--opened-modal");
+});
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.classList.remove("active");
+        document.body.classList.remove("body--opened-modal")
+    }
+}
