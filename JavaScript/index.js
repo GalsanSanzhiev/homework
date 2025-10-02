31f89cc39854d3e2fcce4383d7e608c4c2b4243c
GalsanSanzhiev <galsans054@yahoo.com>
2025-10-02 17:43:19 +0700

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,24 @@
+// 1 task
+const firstName = "Alex";
+const lastName = "Stepanov";
+const isStudent = true;
+
+//2 task
+const age = 19;
+const currentYear = 2025;
+const birthYear = ([currentYear] - [age]);
+// console.log(birthYear);
+
+//3 task
+//console.log("Меня зовут " + firstName + " " + lastName + ", мне " + age + " лет. Я ученик курса: " + isStudent + ".");
+
+//4 task
+
+let a = '123';
+let b = +'456';
+let c = Number('789');
+let d = Boolean(0);
+let e = Boolean(' ');
+let result = a + b + c + d + e;
+console.log(result);
+// 123456789falsetrue
